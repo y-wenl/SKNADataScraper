d4b020517097da066cc662bed5ce128941271df1
Wenli <wenli@ono-sendai>
2020-09-13 18:15:09 -0400

    Initial commit

---
+++ scrape_vote_data.py
@@ -0,0 +1,214 @@
+#! /usr/bin/python
+
+import os
+import re
+import datetime
+import time
+import json
+from vote_scraper_methods import *
+from copy import copy,deepcopy
+
+all_sessions = [20, 21]
+current_session = 21
+
+data_dir = 'data'
+
+########## Update bill lists ##########
+
+# * Don't update past sessions if we have the data
+# * Update current session if data age > 1 week
+bill_list_freshness_age_limit = 7 * 24 * 3600 # freshness limit in seconds
+
+bill_list_data_filename_regex = re.compile('bill_list_data_session([2-9][0-9])_dltime([0-9]+).json')
+bill_list_data_filename_template = 'bill_list_data_session{}_dltime{}.json'
+
+# Figure out which sessions need to be downloaded
+bill_sessions_to_dl = set(all_sessions)
+for filename in os.listdir(data_dir):
+    filepath = os.path.join(data_dir, filename)
+
+    bill_list_data_filename_search = bill_list_data_filename_regex.search(filename)
+    if bill_list_data_filename_search:
+        session = int(bill_list_data_filename_search.group(1))
+
+        if session == current_session:
+            dltime = int(bill_list_data_filename_search.group(2))
+
+            curtime = time.time()
+            age_secs_filename = curtime - dltime
+
+            if age_secs_filename < bill_list_freshness_age_limit:
+                # if bill data is sufficiently fresh, no need to download it again
+                bill_sessions_to_dl -= set([session])
+        else:
+            # if bill data exists for a prior session, no need to download it again
+            bill_sessions_to_dl -= set([session])
+
+
+# Download and save relevant sessions
+curtime = int(time.time())
+for session in bill_sessions_to_dl:
+    bill_list_data = scrape_bill_list_data(session)
+    json_data = json.dumps(bill_list_data, ensure_ascii=False)
+    output_filename = bill_list_data_filename_template.format(session, curtime)
+    output_filepath = os.path.join(data_dir, output_filename)
+    with open(output_filepath, 'w') as f:
+        f.write(json_data)
+        print("Saved to {}.".format(output_filepath))
+
+
+# Load bill_list_data
+bill_list_filepaths = {}
+freshest_bill_list_times = {s:0 for s in all_sessions}
+for filename in os.listdir(data_dir):
+    filepath = os.path.join(data_dir, filename)
+
+    bill_list_data_filename_search = bill_list_data_filename_regex.search(filename)
+    if bill_list_data_filename_search:
+        session = int(bill_list_data_filename_search.group(1))
+        dltime = int(bill_list_data_filename_search.group(2))
+        if dltime > freshest_bill_list_times[session]:
+            bill_list_filepaths[session] = filepath
+            freshest_bill_list_times[session] = dltime
+
+bill_list_datas = {}
+for session in all_sessions:
+    with open(bill_list_filepaths[session], 'r') as f:
+        bill_list_datas[session] = json.load(f)
+
+
+#######################################
+
+########## Update bill data ##########
+
+bill_data_filename_regex = re.compile('bill_data_session([2-9][0-9])_no([^_]*)_id(.*).json')
+bill_data_filename_template = 'bill_list_data_session{}_no{}_id{}.json'
+
+##### Download bill vote data
+bill_data_dir = os.path.join(data_dir, 'bills')
+for session in [21]:#all_sessions:
+    bill_list_data = bill_list_datas[session]
+    bill_list = bill_list_data['resListVo']
+    for bill in bill_list:
+        bill_no = bill['billno']
+        bill_id = bill['billid']
+        bill_id_master = bill['idmaster']
+
+        bill_filename = bill_data_filename_template.format(session, bill_no, bill_id)
+        bill_filepath = os.path.join(bill_data_dir, bill_filename)
+        if not os.path.isfile(bill_filepath):
+            # scrape data and save
+            try:
+            #if True:
+                bill_data = scrape_bill_data(bill_no, bill_id, bill_id_master, session)
+                bill_data['result'] = bill['result']
+                bill_data['name'] = bill['billname']
+                bill_data['kind'] = bill['billkindcd']
+                bill_data['committee'] = bill['currcommitte'] if 'currcommitte' in bill else None
+                with open(bill_filepath, 'w') as f:
+                    json.dump(bill_data, f, ensure_ascii=False)
+            except:
+                donothing = True # probably some download error...
+
+######################################
+
+########## Update member data ##########
+member_list_freshness_age_limit = 30 * 24 * 3600 # freshness limit in seconds
+
+member_list_data_filename_regex = re.compile('member_list_data_session([2-9][0-9])_dltime([0-9]+).json')
+member_list_data_filename_template = 'member_list_data_session{}_dltime{}.json'
+
+member_info_data_filename_regex = re.compile('member_info_data_session([2-9][0-9]).json')
+member_info_data_filename_template = 'member_info_data_session{}.json'
+
+# Figure out which sessions need to be downloaded
+member_sessions_to_dl = set(all_sessions)
+for filename in os.listdir(data_dir):
+    filepath = os.path.join(data_dir, filename)
+
+    member_list_data_filename_search = member_list_data_filename_regex.search(filename)
+    if member_list_data_filename_search:
+        session = int(member_list_data_filename_search.group(1))
+
+        if session == current_session:
+            dltime = int(member_list_data_filename_search.group(2))
+
+            curtime = time.time()
+            age_secs_filename = curtime - dltime
+
+            if age_secs_filename < member_list_freshness_age_limit:
+                # if member data is sufficiently fresh, no need to download it again
+                member_sessions_to_dl -= set([session])
+        else:
+            # if member data exists for a prior session, no need to download it again
+            member_sessions_to_dl -= set([session])
+
+# Download and save relevant sessions
+curtime = int(time.time())
+for session in member_sessions_to_dl:
+    member_list_data = scrape_member_list(session)
+    json_data = json.dumps(member_list_data, ensure_ascii=False)
+    output_filename = member_list_data_filename_template.format(session, curtime)
+    output_filepath = os.path.join(data_dir, output_filename)
+    with open(output_filepath, 'w') as f:
+        f.write(json_data)
+        print("Saved to {}.".format(output_filepath))
+
+# Load member_list_data
+member_list_filepaths = {}
+freshest_member_list_times = {s:0 for s in all_sessions}
+for filename in os.listdir(data_dir):
+    filepath = os.path.join(data_dir, filename)
+
+    member_list_data_filename_search = member_list_data_filename_regex.search(filename)
+    if member_list_data_filename_search:
+        session = int(member_list_data_filename_search.group(1))
+        dltime = int(member_list_data_filename_search.group(2))
+        if dltime > freshest_member_list_times[session]:
+            member_list_filepaths[session] = filepath
+            freshest_member_list_times[session] = dltime
+
+member_list_datas = {}
+for session in all_sessions:
+    with open(member_list_filepaths[session], 'r') as f:
+        member_list_datas[session] = json.load(f)
+
+
+##### Download any missing member info
+
+# load member info files and download any missing data
+maxdl = 10000
+for session in all_sessions:
+    curdl = 0
+    filename = member_info_data_filename_template.format(session)
+    filepath = os.path.join(data_dir, filename)
+
+    # create member info files if they don't exist
+    if not os.path.isfile(filepath):
+        with open(filepath, 'w') as f:
+            json.dump({}, f)
+
+    with open(filepath, 'r') as f:
+        member_info_data = json.load(f)
+
+    existent_member_info_ids = list(member_info_data.keys())
+    
+    for member_id in member_list_datas[session]:
+        if curdl >= maxdl:
+            break
+        if not (member_id in member_info_data):
+            try:
+                member_info_datum = scrape_member_data(member_id, session)
+                member_info_data[member_id] = member_info_datum
+                curdl += 1
+            except:
+                donothing = True # probably some download error.....
+
+    new_member_info_ids = list(member_info_data.keys())
+
+    if len(new_member_info_ids) > len(existent_member_info_ids):
+        print("Saving new member data to {}.".format(filepath))
+        with open(filepath, 'w') as f:
+            json.dump(member_info_data, f, ensure_ascii=False, indent=1)
+
+########################################
---
+++ vote_scraper_methods.py
@@ -0,0 +1,201 @@
+#! /usr/bin/python
+
+import re
+import requests
+from bs4 import BeautifulSoup
+import json
+
+member_list_base = 'http://likms.assembly.go.kr/bill/memVoteResult.do'
+member_data_base = 'http://likms.assembly.go.kr/bill/memVoteDetail.do'
+member_curdata_base = 'https://www.assembly.go.kr/assm/memPop/memPopup.do'
+bill_list_base = 'http://likms.assembly.go.kr/bill/billVoteResult.do'
+bill_data_base = 'http://likms.assembly.go.kr/bill/billVoteResultDetail.do'
+bill_list_ajax_base = 'http://likms.assembly.go.kr/bill/billVoteResultListAjax.do'
+
+def scrape_member_list(session):
+
+    # Get member list page
+    print("Downloading member list #" + str(session) + "...")
+    #website_html = requests.get('{}?ageFrom={}&ageTo={}&age={}'.format(member_list_base, session, session, session)).text
+    website_html = requests.post(member_list_base, data={
+        'ageFrom':  session,
+        'ageTo':    session,
+        'age':      session,
+        }).text
+    soup = BeautifulSoup(website_html,'lxml')
+    print("done")
+
+    # get link for each member
+    member_as = soup.find_all('a', {'href': re.compile('.*:fnViewMemDetail.*')})
+    member_names = [a.text.strip() for a in member_as]
+    member_ids = [re.compile("fnViewMemDetail\('([0-9]+)'").search(a.attrs['href']).group(1) for a in member_as]
+    return {x[0]:x[1] for x in zip(member_ids, member_names)}
+
+def scrape_bill_list_data(session):
+    # get bill list page
+    print("Downloading bill list #" + str(session) + "...")
+    bill_list_json = requests.post(bill_list_ajax_base, data={
+        'ageFrom': session,
+        'ageTo': session,
+        'age': session,
+        'orderType': 'ASC',
+        'strPage': 1,
+        'pageSize': '100000',
+        #'maxPage': '10',
+        'tabMenuType': 'billVoteResult',
+        'searchYn': 'ABC',
+        }).text
+    print("done")
+    bill_list_data = json.loads(bill_list_json)
+
+    return bill_list_data
+
+def scrape_member_data(member_id, session):
+    # Get member data page
+    print("Downloading member data #" + str(member_id) + "...")
+
+    #website_html = requests.get('{}?dept_cd={}'.format(member_curdata_base, member_id)).text # only for current members of the assembly
+    website_html = requests.post(member_data_base, data={
+        'ageFrom':  session,
+        'ageTo':    session,
+        'age':      session,
+        'picDeptCd':member_id,
+        }).text
+    soup = BeautifulSoup(website_html,'lxml')
+    print("done")
+
+    # member name
+    soup_name_info = soup.find('div', {'class':'personName'})
+    member_name = soup_name_info.find('p', {'class':'lang01'}).get_text().strip()
+    member_name_alt = None
+    if soup_name_info.find('p', {'class':'lang02'}):
+        member_name_alt = soup_name_info.find('p', {'class':'lang02'}).get_text().strip()
+
+    # member image url
+    member_image_url = None
+    soup_image_info = soup.find('div', {'class':'person'}).find('img')
+    if soup_image_info:
+        member_image_url = soup_image_info.attrs['src']
+ 
+
+    # member party & district
+    soup_pd_info = soup.find('div', {'class': 'personInfo'}).dl
+    soup_pd_contents = [x for x in soup_pd_info.contents if type(x) == type(soup_pd_info)] # get bs4 tags from the children (eliminating newlines)
+    assert(soup_pd_contents[0].name == 'dt')
+    assert(soup_pd_contents[0].text.strip() == '정당')
+    assert(soup_pd_contents[1].name == 'dd')
+    assert(soup_pd_contents[2].name == 'dt')
+    assert(soup_pd_contents[2].text.strip() == '지역구')
+    assert(soup_pd_contents[3].name == 'dd')
+    member_party = soup_pd_contents[1].text.strip()
+    member_district = soup_pd_contents[3].text.strip()
+
+
+    member_info = {}
+    member_info['name'] = member_name
+    member_info['name_alt'] = member_name_alt
+    member_info['image_url'] = member_image_url
+    member_info['party'] = member_party
+    member_info['district'] = member_district
+    member_info['session'] = session
+    member_info['member_id'] = member_id
+
+    return member_info
+
+def scrape_bill_data(bill_no, bill_id, id_master, session):
+    # Get bill data page
+    print("Downloading bill data #" + bill_id + "...")
+
+    website_html = requests.post(bill_data_base, data={
+        'age':      session,
+        'billNo':   bill_no,
+        'billId':   bill_id,
+        'idMaster': id_master,
+        'tabMenuType': 'billVoteResult',
+        }).text
+    soup = BeautifulSoup(website_html,'lxml')
+    print("done")
+
+    # get summary info
+
+    soup_summary = soup.find('div', {'class':'searchRst'})
+    soup_summary_items = soup_summary.find_all('li')
+    soup_summary_date_item = [x for x in soup_summary_items if '일자' in x.strong.get_text()][0]
+    soup_summary_voters_item = [x for x in soup_summary_items if '표결의원' in x.strong.get_text()][0]
+    soup_summary_result_item = [x for x in soup_summary_items if '표결결과' in x.strong.get_text()][0]
+
+    proposal_date = None
+    vote_date = None
+    date_regex = re.compile('(20[0-9][0-9]-[01]?[0-9]-[0-3]?[0-9])')
+    date_searches = [date_regex.search(s.text) for s in soup_summary_date_item.find_all('span')]
+    if len(date_searches)==2:
+        if date_searches[0]:
+            proposal_date = date_searches[0].group(1)
+        if date_searches[1]:
+            vote_date = date_searches[1].group(1)
+    elif len(date_searches)==1:
+        if date_searches[0]:
+            vote_date = date_searches[0].group(1)
+
+    members_voting = None
+    members_registered = None
+    voters_regex = re.compile('재석\s*([0-9]+)\s*인.*재적\s*([0-9]+)\s*인')
+    voters_search = voters_regex.search(soup_summary_voters_item.span.text)
+    if voters_search:
+        members_voting = int(voters_search.group(1))
+        members_registered = int(voters_search.group(2))
+
+    total_votes = None
+    total_agree = None
+    total_oppose = None
+    total_abstain = None
+    result_regex = re.compile('가결\s*([0-9]+)\s*인.*찬성\s*([0-9]+)\s*인.*반대\s*([0-9]+)\s*인.*기권\s*([0-9]+)\s*인')
+    result_search = result_regex.search(soup_summary_result_item.span.text)
+    if result_search:
+        total_votes = int(result_search.group(1))
+        total_agree = int(result_search.group(2))
+        total_oppose = int(result_search.group(3))
+        total_abstain = int(result_search.group(4))
+
+    # get voter list info
+    soup_box_results = soup.find_all('div', {'class', 'boxResult'})
+    assert(len(soup_box_results) == 3)
+    soup_box_agree = soup_box_results[0]
+    soup_box_oppose = soup_box_results[1]
+    soup_box_abstain = soup_box_results[2]
+    assert('찬성' in soup_box_agree.p.text)
+    assert('반대' in soup_box_oppose.p.text)
+    assert('기권' in soup_box_abstain.p.text)
+
+    agree_member_as = soup_box_agree.find('table', {'class', 'status'}).find_all('a')
+    oppose_member_as = soup_box_oppose.find('table', {'class', 'status'}).find_all('a')
+    abstain_member_as = soup_box_abstain.find('table', {'class', 'status'}).find_all('a')
+
+    id_regex = re.compile("\(['\"]([0-9]+)['\"]\)")
+    get_id_from_str = lambda s: id_regex.search(s).group(1) if id_regex.search(s) else None
+    a_to_pair = lambda a: {'member_id':get_id_from_str(a.attrs['href']), 'name':a.text.strip()}
+
+    agree_member_list = [a_to_pair(a) for a in agree_member_as]
+    oppose_member_list = [a_to_pair(a) for a in oppose_member_as]
+    abstain_member_list = [a_to_pair(a) for a in abstain_member_as]
+
+    #import pdb; pdb.set_trace()
+    assert(len(agree_member_list) == total_agree)
+    assert(len(oppose_member_list) == total_oppose)
+    assert(len(abstain_member_list) == total_abstain)
+
+    bill_data = {}
+    bill_data['proposal_date'] = proposal_date
+    bill_data['vote_date'] = vote_date
+    bill_data['members_voting'] = members_voting
+    bill_data['members_registered'] = members_registered
+    bill_data['total_votes'] = total_votes
+    bill_data['total_agree'] = total_agree
+    bill_data['total_oppose'] = total_oppose
+    bill_data['total_abstain'] = total_abstain
+    bill_data['members_agree'] = agree_member_list
+    bill_data['members_oppose'] = oppose_member_list
+    bill_data['members_abstain'] = abstain_member_list
+
+    return bill_data
+
